28536a23cc07e9960ed08ebaf472f346b0709b4f
Victor Onipe <victoronipe565@gmail.com>
2024-04-19 02:10:55 +0100

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,25 @@
+
+const initApp = () => {
+    const hamburgerBtn = document.getElementById('hamburger-button')
+    const mobileMenu = document.getElementById('mobile-menu')
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden')
+        mobileMenu.classList.toggle('flex')
+        hamburgerBtn.classList.toggle('toggle-btn')
+    }
+
+    hamburgerBtn.addEventListener('click', toggleMenu)
+    mobileMenu.addEventListener('click', toggleMenu)
+}
+
+document.addEventListener('DOMContentLoaded', initApp)
+
+
+function getCurrentYear() {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    return currentYear;
+}
+
+document.getElementById('year').textContent = getCurrentYear(); 
